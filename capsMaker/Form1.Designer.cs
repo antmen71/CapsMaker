8bfbd9aabcfaf02f1a11cc7ff73dbb9d54cb02c2
antmen71 <orkunantmen@hotmail.com>
2015-08-21 09:30:08 +0300

    0.0.0.1

---
+++ capsMaker/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace capsMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resimYukle = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.capsYazisi = new System.Windows.Forms.TextBox();
+            this.capsYap = new System.Windows.Forms.Button();
+            this.resmiKaydet = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // resimYukle
+            // 
+            this.resimYukle.Location = new System.Drawing.Point(8, 13);
+            this.resimYukle.Name = "resimYukle";
+            this.resimYukle.Size = new System.Drawing.Size(88, 23);
+            this.resimYukle.TabIndex = 0;
+            this.resimYukle.Text = "Load Pic";
+            this.resimYukle.UseVisualStyleBackColor = true;
+            this.resimYukle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(741, 412);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Resize += new System.EventHandler(this.pictureBox1_Resize);
+            // 
+            // capsYazisi
+            // 
+            this.capsYazisi.Location = new System.Drawing.Point(81, 42);
+            this.capsYazisi.Name = "capsYazisi";
+            this.capsYazisi.Size = new System.Drawing.Size(341, 20);
+            this.capsYazisi.TabIndex = 2;
+            this.capsYazisi.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // capsYap
+            // 
+            this.capsYap.Location = new System.Drawing.Point(102, 13);
+            this.capsYap.Name = "capsYap";
+            this.capsYap.Size = new System.Drawing.Size(87, 23);
+            this.capsYap.TabIndex = 5;
+            this.capsYap.Text = "CAPS";
+            this.capsYap.UseVisualStyleBackColor = true;
+            this.capsYap.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // resmiKaydet
+            // 
+            this.resmiKaydet.Location = new System.Drawing.Point(195, 13);
+            this.resmiKaydet.Name = "resmiKaydet";
+            this.resmiKaydet.Size = new System.Drawing.Size(87, 23);
+            this.resmiKaydet.TabIndex = 8;
+            this.resmiKaydet.Text = "Save Pic";
+            this.resmiKaydet.UseVisualStyleBackColor = true;
+            this.resmiKaydet.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(356, 15);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(66, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "CAPS text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Font Size";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(86, 10);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(226, 45);
+            this.trackBar1.TabIndex = 12;
+            this.trackBar1.Value = 50;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Jpg Quality";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Location = new System.Drawing.Point(428, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 61);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(186, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Med";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(285, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Max";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Min";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 492);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.resmiKaydet);
+            this.Controls.Add(this.capsYap);
+            this.Controls.Add(this.capsYazisi);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.resimYukle);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "CAPSle";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button resimYukle;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox capsYazisi;
+        private System.Windows.Forms.Button capsYap;
+        private System.Windows.Forms.Button resmiKaydet;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ capsMaker/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+
+namespace capsMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        bool resimVarmi;
+        bool kirmiziFon;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap bit = new Bitmap(open.FileName);
+                    pictureBox1.Image = bit;
+                    int yukseklik;
+                    int genislik;
+                    yukseklik = bit.Height;
+                    genislik = bit.Width;
+                    resimVarmi = true;
+
+
+
+                    if (bit.Height > pictureBox1.Height && bit.Width > pictureBox1.Width)
+                    {
+
+                        pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+
+                    }
+                    else
+                    {
+
+                        pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                throw new ApplicationException("Resim Yüklenemedi");
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (resimVarmi == true)
+            {
+
+                int yukseklik = pictureBox1.Image.Height;
+                int yukseklik1 = pictureBox1.Height;
+                int bantYukseklik = yukseklik / 100 * 20;
+                int genislik = pictureBox1.Image.Width;
+                int genislik1 = pictureBox1.Width;
+
+                Pen PenStyle = new Pen(Color.Red, 10);
+                Rectangle rect = new Rectangle(0, yukseklik - bantYukseklik, genislik, bantYukseklik);
+                Graphics g = pictureBox1.CreateGraphics();
+                System.Drawing.SolidBrush myBrush;
+                myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+                float fontSize = (float)numericUpDown1.Value;
+
+
+
+
+                using (Graphics grp = Graphics.FromImage(pictureBox1.Image))
+                {
+                    grp.FillRectangle(myBrush, rect);
+                    kirmiziFon = true;
+                    //TextFormatFlags flags = TextFormatFlags.WordBreak;
+                    StringFormat stringFormat = new StringFormat();
+                    stringFormat.Alignment = StringAlignment.Center;
+                    stringFormat.LineAlignment = StringAlignment.Center;
+                    SizeF sizF = new SizeF(rect.Width, rect.Height);
+                    PointF poinF = new PointF(0, yukseklik - bantYukseklik);
+                    RectangleF recF = new RectangleF(poinF, sizF);
+
+                    grp.DrawString(capsYazisi.Text, new Font("Comic Sans MS", fontSize), new SolidBrush(Color.White), recF, stringFormat);
+                }
+
+                pictureBox1.Refresh();
+                myBrush.Dispose();
+                g.Dispose();
+                PenStyle.Dispose();
+            }
+            else
+            {
+
+                MessageBox.Show("Lütfen bir resim yükleyiniz");
+            }
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (resimVarmi == true)
+            {
+                saveFileDialog1.Filter = "Jpg Dosyası(*.jpg; *.jpeg)|*.jpg; *.jpeg|Gif Dosyası(*.gif)|*.gif|Bmp Dosyası (*.bmp)|*.bmp";
+
+                saveFileDialog1.AddExtension = true;
+                saveFileDialog1.FileName = "*.jpg";
+
+                DialogResult dialogResult = saveFileDialog1.ShowDialog();
+
+                if (dialogResult == DialogResult.OK)
+                {
+                    Bitmap grp = new Bitmap(pictureBox1.Image);
+
+                    ImageCodecInfo jgpEncoder = GetEncoder(ImageFormat.Jpeg);
+                    System.Drawing.Imaging.Encoder myEncoder =
+                    System.Drawing.Imaging.Encoder.Quality;
+
+                    EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+                    EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, trackBar1.Value);
+                    myEncoderParameters.Param[0] = myEncoderParameter;
+
+                    grp.Save(saveFileDialog1.FileName, jgpEncoder, myEncoderParameters);
+
+                }
+                else if (dialogResult == DialogResult.Cancel )
+                { }
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir resim yükleyiniz");
+            }
+
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+
+
+
+            if (resimVarmi == false)
+            {
+
+            }
+
+            else
+            {
+
+                int yukseklik = pictureBox1.Image.Height;
+                int yukseklik1 = pictureBox1.Height;
+                int bantYukseklik = yukseklik / 100 * 20;
+                int genislik = pictureBox1.Image.Width;
+                int genislik1 = pictureBox1.Width;
+
+                Pen PenStyle = new Pen(Color.Red, 10);
+                Rectangle rect = new Rectangle(0, yukseklik - bantYukseklik, genislik, bantYukseklik);
+                Graphics g = pictureBox1.CreateGraphics();
+                System.Drawing.SolidBrush myBrush;
+                myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+                float fontSize = (float)numericUpDown1.Value;
+                StringFormat stringFormat = new StringFormat();
+                stringFormat.Alignment = StringAlignment.Center;
+                stringFormat.LineAlignment = StringAlignment.Center;
+                SizeF sizF = new SizeF(rect.Width, rect.Height);
+                PointF poinF = new PointF(0, yukseklik - bantYukseklik);
+                RectangleF recF = new RectangleF(poinF, sizF);
+                using (Graphics grp = Graphics.FromImage(pictureBox1.Image))
+                {
+                    grp.FillRectangle(myBrush, rect);
+                    grp.DrawString(capsYazisi.Text, new Font("Comic Sans MS", fontSize), new SolidBrush(Color.White), recF, stringFormat);
+                    pictureBox1.Refresh();
+                    myBrush.Dispose();
+                    g.Dispose();
+                    PenStyle.Dispose();
+
+                }
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+            if (kirmiziFon == false)
+            {
+
+
+            }
+
+            else
+            {
+                int yukseklik = pictureBox1.Image.Height;
+                int yukseklik1 = pictureBox1.Height;
+                int bantYukseklik = yukseklik / 100 * 20;
+                int genislik = pictureBox1.Image.Width;
+                int genislik1 = pictureBox1.Width;
+
+
+                Pen PenStyle = new Pen(Color.Red, 10);
+                Rectangle rect = new Rectangle(0, yukseklik - bantYukseklik, genislik, bantYukseklik);
+                Graphics g = pictureBox1.CreateGraphics();
+                System.Drawing.SolidBrush myBrush;
+                myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+                float fontSize = (float)numericUpDown1.Value;
+
+
+                StringFormat stringFormat = new StringFormat();
+                stringFormat.Alignment = StringAlignment.Center;
+                stringFormat.LineAlignment = StringAlignment.Center;
+
+
+                SizeF sizF = new SizeF(rect.Width, rect.Height);
+                PointF poinF = new PointF(0, yukseklik - bantYukseklik);
+                RectangleF recF = new RectangleF(poinF, sizF);
+                using (Graphics grp = Graphics.FromImage(pictureBox1.Image))
+                {
+                    grp.FillRectangle(myBrush, rect);
+                    grp.DrawString(capsYazisi.Text, new Font("Comic Sans MS", fontSize), new SolidBrush(Color.White), recF, stringFormat);
+                    pictureBox1.Refresh();
+                    myBrush.Dispose();
+                    g.Dispose();
+                    PenStyle.Dispose();
+
+                }
+                //using (Graphics grp1 = Graphics.FromImage(pictureBox1.Image))
+                //{
+                //    grp1.FillRectangle(myBrush, rect);
+                //    grp1.DrawString(capsYazisi.Text, new Font(fontDialog1.Font.Name, fontSize), new SolidBrush(Color.White), 20, yukseklik - bantYukseklik + 5);
+                //    pictureBox1.Refresh();
+
+                //    myBrush.Dispose();
+                //    g.Dispose();
+                //    PenStyle.Dispose();
+                //}
+
+
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
+
